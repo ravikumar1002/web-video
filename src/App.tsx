fb82fb0c64baa3d1e9af130713ba1cd61f8ffa6b
Ravi kumar <1002kumarravi@gmail.com>
2022-12-04 12:24:52 +0530

    init setup

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+import { initializeApp } from "firebase/app";
+import { config } from "./config/config";
+initializeApp(config.firebaseConfig);
+
+function App() {
+
+  return <div className="App">
+    
+  </div>;
+}
+
+export default App;
---
+++ src/config/config.ts
@@ -0,0 +1,10 @@
+export const config = {
+    firebaseConfig: {
+        apiKey: "AIzaSyB32iYJWd_0s7cr7q2O-_Fhc3Fu_e3FM7U",
+        authDomain: "web-video-af0ad.firebaseapp.com",
+        projectId: "web-video-af0ad",
+        storageBucket: "web-video-af0ad.appspot.com",
+        messagingSenderId: "411298889980",
+        appId: "1:411298889980:web:e649889d5921a1ed98d779"
+    }
+}
