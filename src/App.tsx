e597763a0af0916009ca9f89b1bb1d2168f34850
Ravi kumar <1002kumarravi@gmail.com>
2022-12-09 00:06:26 +0530

    login and signup page ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,36 @@
-import { useState } from "react";
-import "./App.css";
+// import "./App.css";
 import { initializeApp } from "firebase/app";
 import { config } from "./config/config";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { HomePage } from "./pages/home";
+import SignupPage from "./pages/auth/SignUp";
+import { LoginPage } from "./pages/auth/Login";
+
 initializeApp(config.firebaseConfig);
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <SignupPage />,
+      children: [
+        {
+          path: "/login",
+          element: <LoginPage />,
+        },
+        {
+          path: "/signup",
+          element: <SignupPage />,
+        },
+      ],
+    },
+  ]);
 
-  return <div className="App">
-    
-  </div>;
+  return (
+    <div className="App">
+      <RouterProvider router={router} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,9 @@
+
+export const  HomePage = () => {
+
+    return(
+        <div>
+            <h2>Home</h2>
+        </div>
+    )
+}
