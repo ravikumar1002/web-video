9bab9b81862765e497ea44afd11e9dec6e53aa11
Ravi kumar <1002kumarravi@gmail.com>
2023-01-05 17:11:54 +0530

    unwated svg

--- src/assets/index.ts
+++ src/assets/index.ts
@@ -1,15 +1,10 @@
 import MenuLogo from "./menu.svg";
-import ClockLogo from "./clock.svg";
-import HeartLogo from "./heart.svg";
-import HomeLogo from "./home.svg";
-import PlaylistsLogo from "./playlist.svg";
 import BackArrow from "./back-arrow.svg"
-import HistoryLogo from "./history.svg"
 import GoogleLogo from "./google.svg"
 import DeleteLogo from "./delete-button.svg"
 import GitHubLogo from "./github.svg"
 
-export { MenuLogo, ClockLogo, HeartLogo, HomeLogo, PlaylistsLogo, BackArrow, HistoryLogo, DeleteLogo, GoogleLogo, GitHubLogo }
+export { MenuLogo, BackArrow, DeleteLogo, GoogleLogo, GitHubLogo }
 
 
 
